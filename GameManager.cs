06726ac7a54ba4e276cc560783c576a52b95bb41
davidjorge18 <34067951+davidjorge18@users.noreply.github.com>
2018-11-02 18:24:30 +0000

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    
+    public float turnDelay = .1f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerHP = 20;
+    [HideInInspector] public bool playersTurn = true;
+    
+    private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+        enemies.Clear();
+    }
+    
+    public void GameOver()
+    {
+        enabled = false;
+    }
+    
+    
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving=true;
+        yield return new WaitForSeconds(turnDelay);
+        
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+            
+            
+        }
+        
+        playersTurn = true;
+        enemiesMoving = false;
+        
+        
+    }
+    
+    
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(playersTurn || enemiesMoving)
+            return;
+        
+        StartCoroutine(MoveEnemies());
+        
+		
+	}
+    
+    public void AddEnemyToList(Enemy script)
+    {
+        
+        enemies.Add (script);
+        
+    }
+    
+    
+    
+}
---
+++ Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject {
+    
+    public int wallDamage = 1;
+    public int enemyDamage = 2;
+    
+    public float restartLevelDelay = 1f;
+    
+    private Animator animator;
+    private int hp;
+    
+	// Use this for initialization
+	protected override void Start () {
+		
+        animator = GetComponent<Animator>();
+        
+        hp = GameManager.instance.playerHP;
+        
+        base.Start();
+        
+        
+	}
+    
+    private void OnDisable()
+    {
+        
+        GameManager.instance.playerHP = hp;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(!GameManager.instance.playersTurn) return;
+        
+        int horizontal = 0;
+        int vertical = 0;
+        
+        
+        horizontal = (int) Input.GetAxisRaw("Horizontal");
+        vertical = (int) Input.GetAxisRaw("Vertical");
+        
+        if(horizontal != 0)
+            vertical = 0;
+        
+        
+        if(horizontal != 0 || vertical != 0)
+            AttemptMove<Enemy> (horizontal, vertical);
+        
+	}
+    
+    protected override void AttemptMove <T> (int xDir, int yDir)
+    {
+        base.AttemptMove <T> (xDir, yDir);
+        
+        RaycastHit2D hit;
+        
+        CheckIfGameOver();
+        
+        GameManager.instance.playersTurn = false;
+    }
+    
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+    }
+    
+    
+    
+    protected override void OnCantMove <T> (T component)
+    {
+        Enemy hitEnemy = component as Enemy;
+        hitEnemy.DamageEnemy(enemyDamage);
+        animator.SetTrigger("playerHit");
+        
+    }
+    
+    private void Restart()
+    {
+        SceneManager.LoadScene (0);
+    }
+    
+    private void CheckIfGameOver()
+    {
+        
+        if(hp<=0)
+            GameManager.instance.GameOver();
+        
+    }
+    
+    public void LoseHp(int loss)
+    {
+        animator.SetTrigger("playerDmgd");
+        hp -= loss;
+        CheckIfGameOver();
+    }
+    
+}
